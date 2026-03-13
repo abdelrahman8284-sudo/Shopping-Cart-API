c47c54083a40dc2546cd7c5b41e86492ee215415
Abdo Ahmed <abdoahmed@gmail.com>
2026-03-13 16:25:50 +0200

    first commit

---
+++ src/main/java/com/abdelrahman/shoppingcart/ShoppingCartApiApplication.java
@@ -0,0 +1,13 @@
+package com.abdelrahman.shoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartApiApplication.class, args);
+	}
+
+}
